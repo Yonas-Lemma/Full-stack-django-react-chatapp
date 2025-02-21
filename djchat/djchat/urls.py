d43408336aa6e48099c0062b5c788b0bd28e7b98
[Yonas <Yonbetizazu@gmail.com>
2025-02-21 21:23:58 +0100

    create the model

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-
-
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/doc/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/doc/schema/ui/", SpectacularSwaggerView.as_view()),
 ]
